40aa34b3001e8847e3b091fd8b10b9734a90e196
TsaplinIA <tsaplin.ia@outlook.com>
2024-12-10 16:13:17 +0300

    add set constants endpoint

---
+++ app/api/constants.py
@@ -0,0 +1,28 @@
+from fastapi import HTTPException, APIRouter
+from pydantic import BaseModel, ValidationError
+from typing import Any
+
+from app.config import constants
+
+
+class ResponseModel(BaseModel):
+    message: str
+
+
+constants_router = APIRouter(prefix="/constants", tags=["constants"])
+
+
+@constants_router.post("/", response_model=ResponseModel, status_code=200)
+async def validate_fields(input_data: dict[str, Any]):
+    valid_data = {}
+    for key, value in input_data.items():
+        if not hasattr(constants, key):
+            raise HTTPException(status_code=422, detail=f"Constant '{key}' does not exist.")
+        try:
+            constants.model_validate({key: value}, strict=True)
+        except ValidationError:
+            raise HTTPException(status_code=422, detail=f"Constant '{key}' can't be {value}.")
+        valid_data[key] = value
+    for key, value in valid_data.items():
+        setattr(constants, key, value)
+    return {"message": "OK"}
