016dbd724fdb6b0bedf5b7d31d05d86339ef2977
TsaplinIA <tsaplin.ia@outlook.com>
2024-12-15 06:33:50 +0300

    add missed imports

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
+import os
 from contextlib import asynccontextmanager
 
+import socketio
 from fastapi import FastAPI
 
 from app.admin import admin
 from app.api.constants import constants_router
+from app.api.gsi import gsi_router
+from app.api.players import players_router
+from app.api.teams import teams_router
+from app.config import get_assets_dir, get_huds_dir
 from app.database import Base, engine
 from fastapi.staticfiles import StaticFiles
 
@@ -38,7 +44,11 @@ def init_app():
     app.mount("/files", StaticFiles(directory=os.path.join(get_assets_dir(), "files")), name="files")
 
     app.include_router(constants_router)
+    app.include_router(gsi_router)
     app.include_router(hud_router)
+    app.include_router(teams_router, prefix="/api")
+    app.include_router(players_router, prefix="/api")
+
     admin.mount_to(app)
     return app
 
