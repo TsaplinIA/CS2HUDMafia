189150aed58ce62fb05b89a0bda4c9ca6b00672b
TsaplinIA <tsaplin.ia@outlook.com>
2025-11-16 12:37:03 +0300

    add players to db if they connect to server

--- app/api/gsi.py
+++ app/api/gsi.py
@@ -21,7 +21,7 @@ unexpected_fields_global = set()
 
 @gsi_router.post("/hud", response_model=ResponseModel, status_code=200)
 async def listen_gsi(request: Request, input_data: dict[str, Any]):
-    # await request.app.sio.emit("update", input_data)
+    await request.app.sio.emit("update", input_data)
     js = await request.body()
 
     gsi_dict = json.loads(js)
@@ -39,7 +39,36 @@ async def listen_gsi(request: Request, input_data: dict[str, Any]):
         if 'added' in unexpected_field:
             print(gsi_dict['added'])
     try:
-        gsi_obj = hud_gsi_decoder.decode(js)
-    except msgspec.ValidationError as e:
-        print(e, js)
+        gsi_obj: HUDGSI = hud_gsi_decoder.decode(js)
+    except (msgspec.ValidationError, Exception) as e:
+        return {"message": str(e)}
+    if gsi_obj.allplayers and isinstance(gsi_obj.allplayers, dict):
+        await sync_player_list(request, gsi_obj.allplayers)
+
     return {"message": "OK"}
+
+
+async def sync_player_list(request: Request, players: dict[str, GSIPlayer]):
+    players_set = set(players.keys())
+    for player in request.app.player_list:
+        if player not in players_set:
+            request.app.player_list.remove(player)
+        else:
+            players_set.remove(player)
+    if not players_set:
+        return
+    with get_session() as session:
+        existed_players = session.query(DBPlayer.id).filter(DBPlayer.id.in_(players_set)).all()
+        existed_players = {pid for (pid,) in existed_players}
+
+        for steam_id in players_set:
+            request.app.player_list.add(steam_id)
+            if steam_id in existed_players:
+                continue
+            gsi_player = players[steam_id]
+            db_player = DBPlayer(
+                steam_id=steam_id,
+                steam_name=gsi_player.name,
+            )
+            session.add(db_player)
+        session.commit()
